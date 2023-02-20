13f3d50658b6efc9e88bfd2b95d1583cd7d13093
ashish-sah <ashish.sah@pubmatic.com>
2023-02-20 22:53:37 +0530

    Created Server

---
+++ database.js
@@ -0,0 +1,49 @@
+import sqlite from 'sqlite3';
+
+export function createTable(db) {
+    const createStudentTable = `CREATE TABLE Student (
+        studentId INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        rollNo TEXT NOT NULL,
+        department TEXT NOT NULL,
+        batch TEXT NOT NULL,
+        phoneNumber TEXT NOT NULL,
+        currentYear TEXT NOT NULL,
+        photoUrl TEXT NOT NULL,
+        createdAt TEXT DEFAULT CURRENT_TIMESTAMP
+    )`;
+
+    const createTeacherTable = `CREATE TABLE Teacher (
+        teacherId INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        department TEXT NOT NULL,
+        phoneNumber TEXT NOT NULL,
+        photoUrl TEXT NOT NULL,
+        createdAt TEXT DEFAULT CURRENT_TIMESTAMP
+    )`;
+
+    const createAttendanceTable = `CREATE TABLE Attendance (
+        attendanceId INTEGER PRIMARY KEY AUTOINCREMENT,
+        classYear TEXT NOT NULL,
+        classDept TEXT NOT NULL,
+        classSubject TEXT NOT NULL,
+        startTime INTEGER NOT NULL,
+        students TEXT,
+        duration INTEGER NOT NULL,
+        teacherId INTEGER REFERENCES Teacher(teacherId),
+        UNIQUE (attendanceId, classYear, classDept, classSubject, startTime, duration) ON CONFLICT REPLACE
+    )`;
+
+    // db.run(createStudentTable);
+    // db.run(createTeacherTable);
+    // db.run(createAttendanceTable);
+}
+
+export const db = new sqlite.Database("./data.db", sqlite.OPEN_READWRITE, (err) => {
+    if (err) console.error(err);
+    console.log("Connected to DB");
+});
---
+++ test.py
@@ -0,0 +1,28 @@
+import config
+import torch.optim as optim
+import torch.nn.functional as F
+from model import SiameseNetwork
+from utils import load_checkpoint
+from PIL import Image, ImageOps
+
+model = SiameseNetwork().to(config.DEVICE)
+optimizer = optim.Adam(model.parameters(),lr=config.LR)
+load_checkpoint("my_checkpoint.pth.tar",model,optimizer,config.LR)
+
+image1 = Image.open('images/image1.png')
+image1 = ImageOps.grayscale(image1)
+image1 = image1.convert('L')
+image1 = config.TRANS(image1)
+image1 = image1.unsqueeze(0)
+image2 = Image.open('images/image2.png')
+image2 = ImageOps.grayscale(image2)
+image2 = image2.convert('L')
+image2 = config.TRANS(image2)
+image2 = image2.unsqueeze(0)
+
+output1,output2 = model(image1.to(config.DEVICE),image2.to(config.DEVICE))
+euclidean_distance = F.pairwise_distance(output1, output2)
+if euclidean_distance.item() < 1 :
+    print(1)
+else :
+    print(0)
